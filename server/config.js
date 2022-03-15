6dfd01e5c12d4e63a4ecf4cdba0eb886050c7b7a
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-03-14 23:05:42 -0300

    backend folders and index

---
+++ server/config.js
@@ -0,0 +1,18 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const root = join( currentDir, '../');
+const audioDirectory = join( root, 'audio');
+const publicDirectory = join( root, 'public');
+
+export default {
+    port:   process.env.PORT || 3000,
+    dir: {
+        root,
+        publicDirectory,
+        audioDirectory,
+        songs: join(audioDirectory, 'songs'),
+        fxDirectory: join(publicDirectory, 'fx')
+    }
+}
